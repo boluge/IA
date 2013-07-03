8ecf8d55ecbba75f28e39926cd2b1f487a5dc943
Stéphane <boluge@gmail.com>
2013-07-03 14:37:28 +0200

    up

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 ﻿<!DOCTYPE html>
 <html>
 	<head>
-		<title>TPE sur l'Intelligence Artificielle 2010-2011</title>
+		<title>TPE sur l&acute;Intelligence Artificielle 2010-2011</title>
 		<link rel="stylesheet" href="assets/css/style.css" />
 		<meta charset="utf-8" />
 	</head>
 	<body>
 		<?php include "assets/elements/header.html"; ?>
 		<div class="content">
-			<h1>L'Intelligence Artificielle.</h1>
+			<h1>L&acute;Intelligence Artificielle.</h1>
 
-			<p>Durant l'année de première, nous avons dû réaliser un TPE (Travail Personnel Encadré). Nous avons choisis l'Intelligence Artificielle.</p>
-			<p>Voici un petit aperçu de ce que nous avons appris !<br /> Avant toute chose voici  <a href="http://fr.wikipedia.org/wiki/Intelligence_artificielle">la définition donnée par Wikipédia</a>. Pour résumer, celle ci nous dit que l'intelligence artificielle est la somme de différents moyens grâce auxquels les ordinateur pourraient un jour penser.</p>
+			<p>Durant l&acute;ann&eacute;e de premi&egrave;re, nous avons dû r&eacute;aliser un TPE (Travail Personnel Encadr&eacute;). Nous avons choisis l'Intelligence Artificielle.</p>
+			<p>Voici un petit aper&ccedil;u de ce que nous avons appris !<br /> Avant toute chose voici  <a href="http://fr.wikipedia.org/wiki/Intelligence_artificielle">la d&eacute;finition donn&eacute;e par Wikip&eacute;dia</a>. Pour r&eacute;sumer, celle ci nous dit que l'intelligence artificielle est la somme de diff&eacute;rents moyens grâce auxquels les ordinateur pourraient un jour penser.</p>
 			<center><img src="http://placehold.it/400x350" title="Un robot joue à papier caillou ciseau avec un homme" alt="Un robot joue à papier caillou ciseau avec un homme" /></center>
 			<br />
 			<p>
-				<small><u>Note:</u> Ce site est encore en construction, pour l'instant le fond est préféré à la forme.</small><br /><br />
+				<small><u>Note:</u> Ce site est encore en construction, pour l&acute;instant le fond est pr&eacute;f&eacute;r&eacute; à la forme.</small><br /><br />
 				<a href="page1.php">Introduction</a>
 			</p>
 		</div>	
