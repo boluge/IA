45115a9f4e706d5de16ae3e4952fd97eee512935
Stéphane <boluge@gmail.com>
2013-07-03 14:30:54 +0200

    mise à jour

---
+++ index.php
@@ -0,0 +1,28 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<title>TPE sur l'Intelligence Artificielle 2010-2011</title>
+		<link rel="stylesheet" href="assets/css/style.css" />
+		<meta charset="utf-8" />
+	</head>
+	<body>
+		<?php include'assets/elements/header.html'; ?>
+		<div class="content">
+			<h1>
+				<center>
+				L'Intelligence Artificielle.
+				</center>
+			</h1>
+
+			<p>Durant l'année de première, nous avons dû réaliser un TPE (Travail Personnel Encadré). Nous avons choisis l'Intelligence Artificielle.</p>
+			<p>Voici un petit aperçu de ce que nous avons appris !<br /> Avant toute chose voici  <a href="http://fr.wikipedia.org/wiki/Intelligence_artificielle">la définition donnée par Wikipédia</a>. Pour résumer, celle ci nous dit que l'intelligence artificielle est la somme de différents moyens grâce auxquels les ordinateur pourraient un jour penser.</p>
+			<center><img src="http://placehold.it/400x350" title="Un robot joue à papier caillou ciseau avec un homme" alt="Un robot joue à papier caillou ciseau avec un homme" /></center>
+			<br />
+			<p>
+				<small><u>Note:</u> Ce site est encore en construction, pour l'instant le fond est préféré à la forme.</small><br /><br />
+				<a href="page1.php">Introduction</a>
+			</p>
+		</div>	
+		<?php include "assets/elements/footer.html"; ?>
+	</body>
+</html>
---
+++ page1.php
@@ -0,0 +1,20 @@
+﻿<?php require_once('assets/elements/header.php'); ?>
+
+	<div class="content">
+		<h1>
+			<center>
+			L'Intelligence Artificielle.
+			</center>
+		</h1>
+
+		<p>Durant l'année de première, nous avons dû réaliser un TPE (Travail Personnel Encadré). Nous avons choisis l'Intelligence Artificielle.</p>
+		<p>Voici un petit aperçu de ce que nous avons appris !<br /> Avant toute chose voici  <a href="http://fr.wikipedia.org/wiki/Intelligence_artificielle">la définition donnée par Wikipédia</a>. Pour résumer, celle ci nous dit que l'intelligence artificielle est la somme de différents moyens grâce auxquels les ordinateur pourraient un jour penser.</p>
+		<center><img src="images/shifumi_robot_homme.jpg" title="Un robot joue à papier caillou ciseau avec un homme" alt="Un robot joue à papier caillou ciseau avec un homme" /></center>
+		<br />
+		<p>
+			<small><u>Note:</u> Ce site est encore en construction, pour l'instant le fond est préféré à la forme.</small><br /><br />
+			<a href="page1.html">Introduction</a>
+		</p>
+	</div>
+	
+<?php require_once('assets/elements/footer.php'); ?>
